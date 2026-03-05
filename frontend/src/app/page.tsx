29d2ac8b62652359c278843e06dc212b2336676c
Anton <garynets.anton@gmail.com>
2026-03-05 11:39:47 +0200

    chore(project): initial commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+export default async function Home() {
+    const data = await fetch(`${process.env.API_URL}/test/`);
+    const text: { text: string } = await data.json();
+    return (
+        <Typography variant="h1" component="h2">
+            {text.text}
+        </Typography>
+    );
+}
